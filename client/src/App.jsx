926607f4d777fcc92d7d08b71b3f437e38682994
Nhat Nguyen <nguyentvnhat1411@gmail.com>
2022-01-29 23:36:26 +0700

    completed the Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import { Footer, Navbar, Services, Transactions, Welcome } from "./components"
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar/>
+        <Welcome/>
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
     </div>
   )
 }
